4df27b6e7dec3f3bb01a54837a8bcfa41ad42322
Ramiro <ramirovazquez95@gmail.com>
2017-09-22 15:48:28 -0500

    dia 4

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -7,3 +7,14 @@ let sauce = "Eggs and cheese"; // ES6 syntax
 // Template literals, like the one below, were introduced in ES6
 const carbonara = `You can make carbonara with ${pasta}, ${meat}, and a sauce made with ${sauce}. ramiro b`;
 
+const blankLine = '  |   |  ';
+console.log("This is what an empty board would look like:");
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+const guessLine = '1 |   |  ';
+const bombLine  = '  | B |  ';
+console.log("This is what a board with a guess and a bomb on it would look like:");
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
