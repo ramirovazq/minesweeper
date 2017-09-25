87ca443ef2c96de868c6a73a862f2c604c59e7c4
Ramiro <ramirovazquez95@gmail.com>
2017-09-25 15:28:41 -0500

    cambios

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,20 +1,13 @@
-var pasta = "Spaghetti"; // ES5 syntax
+const printBoard = (board) => {
+    console.log("Current Board:");
+    console.log( board[0].join(' | '));
+    console.log( board[1].join(' | '));
+    console.log( board[2].join(' | '));
+    return ""
+};
 
-const meat = "Pancetta"; // ES6 syntax
-
-let sauce = "Eggs and cheese"; // ES6 syntax
-
-// Template literals, like the one below, were introduced in ES6
-const carbonara = `You can make carbonara with ${pasta}, ${meat}, and a sauce made with ${sauce}. ramiro b`;
-
-const blankLine = '  |   |  ';
-console.log("This is what an empty board would look like:");
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
-const guessLine = '1 |   |  ';
-const bombLine  = '  | B |  ';
-console.log("This is what a board with a guess and a bomb on it would look like:");
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+let board = [[' ',' ', ' '],[' ', ' ', ' '],[' ', ' ', ' ']];
+console.log(printBoard(board));
+board[0][1] = '1';
+board[2][2] = 'B';
+console.log(printBoard(board));
